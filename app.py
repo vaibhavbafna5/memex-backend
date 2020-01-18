f5b3639ae70d15905b8fc6daf5e81f2cae1d94f2
Vaibhav Bafna <vbafna@umich.edu>
2020-01-18 03:55:44 -0500

    merp

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def show_user_entries():
 
 @app.route('user/<uuid>/add', methods=['POST'])
 def add_entry_for_user():
-    return 'User %s' % escape(uuid)
+    return 'success + {uuid}'
 
     # add to SQL schema
 
