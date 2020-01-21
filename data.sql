6c5a12719e12cb5574c321c54e6fb173a4142ddd
Vaibhav Bafna <vbafna@umich.edu>
2020-01-21 11:22:41 -0500

    merp

--- data.sql
+++ data.sql
@@ -1,8 +1,8 @@
-INSERT INTO users(uuid, username, email, password)
-VALUES ('1', 'vbafna', 'vbafna@umich.edu', 'merpmerp'),
-('2', 'rprasad', 'rishipr@umich.edu', 'merpmerp');
+INSERT INTO users(username, email, password)
+VALUES ('vbafna', 'vbafna@umich.edu', 'merpmerp'),
+('rprasad', 'rishipr@umich.edu', 'merpmerp');
 
-INSERT INTO entries(entryid, owner, link, title, snippet, usernotes)
-VALUES ('100', '1', 'google.com', 'Google', 'Merp Merp', 'Nothing to say'),
-('200', '1', 'yahoo.com', 'Yahoo', 'Perm Perm', 'Nothing to say here either'),
-('300', '2', 'twitter.com', 'Twitter', 'Where its all going down', '');
+INSERT INTO entries(owner, link, title, snippet, usernotes)
+VALUES ('1', 'google.com', 'Google', 'Merp Merp', 'Nothing to say'),
+('1', 'yahoo.com', 'Yahoo', 'Perm Perm', 'Nothing to say here either'),
+('2', 'twitter.com', 'Twitter', 'Where its all going down', '');
